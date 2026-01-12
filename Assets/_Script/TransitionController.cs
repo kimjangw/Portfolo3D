f73bd2d99941fb2d560f09cd28b27f4d5f560f44
kimjangw <kimjangw95@gmail.com>
2026-01-12 09:30:58 +0900

    중간저장
    중간저장

--- Assets/_Script/TransitionController.cs
+++ Assets/_Script/TransitionController.cs
@@ -29,24 +29,26 @@ public class TransitionController : MonoBehaviour
     {
         isTransitioning = true;
 
-        // 기준축을 월드 원점 기준으로 미러 (z + x)
+        // 1) 월드 기준 X/Z 미러링
         Vector3 pos = player.position;
-        pos = new Vector3(-pos.x, pos.y, -pos.z);
+        pos.x = -pos.x;
+        pos.z = -pos.z;
         player.position = pos;
 
-        // 회전 180
+        // 2) yaw 180 회전
         float newYaw = cameraController.yaw + 180f;
         cameraController.yaw = newYaw;
         player.rotation = Quaternion.Euler(0f, newYaw, 0f);
 
-        // 카메라 스냅
+        // 3) 카메라 스냅 (yaw는 건드리지 않음)
         cameraController.SnapToPlayerInstant();
 
-        // 재트리거 방지
+        // 4) 재트리거 방지용 offset
         player.position -= player.forward * 0.1f;
 
         StartCoroutine(ResetFlag());
     }
+
 
 
 
